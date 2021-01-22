eb84bff0e0b3d7cead8094d25d47f0c54cb9596d
bdrkreza <diureza@gamil.com>
2021-01-22 19:00:06 +0600

    update

--- assignment.js
+++ assignment.js
@@ -1,16 +1,19 @@
-//github link:
+//github link: https://github.com/bdrkreza/assignmen
 
 
-//megafriend program 
+//megafriend program (1)
+
+// Array call
 let Friendname = [
     "rkreza",
     "mofassel",
     "monirHossian",
     "kalam"
 ];
-
+// function call
 function megaFriend(Friendname) {
     let longest = Friendname[0];
+
     for (let i = 0; i < Friendname.length; i++) {
         if (Friendname[i].length > longest.length) {
             longest = Friendname[i];
@@ -19,3 +22,24 @@ function megaFriend(Friendname) {
     return longest;
 }
 console.log(megaFriend(Friendname));
+//megafriend program End
+
+// budgetCalculator program (2)
+
+function budgetCalculator(watch, laptop, mobail) {
+    let result = watch + laptop + mobail;
+    return result;
+}
+let total = budgetCalculator(50, 500, 100);
+console.log(total);
+// budgetCalculator program End
+
+// kilometerToMeter program  (3)
+function kilometerToMeter(kilometer) {
+    let meter = 1000;
+    let result = kilometer * meter;
+    return result;
+}
+let convert = kilometerToMeter(1);
+console.log(convert);
+// kilometerToMeter program End
